d1a781828e6a51655dc95873c710a78f98af3e28
sarzig <63985693+sarzig@users.noreply.github.com>
2024-04-14 11:47:40 -0400

    changed the app.listen to allow online access

--- App.js
+++ App.js
@@ -12,4 +12,11 @@ CourseRoutes(app);
 Lab5(app);
 Hello(app);
 
-app.listen(4001);
+/*
+To host our Node HTTP server on a remote server we need to make a few tweaks. For instance, 
+we can't use port 4000 like we do in our local environment. Instead, we need to use the 
+port declared in an environment variable called PORT available through process.env.PORT. 
+In your Node application, refactor App.js so that it uses the PORT environment variable if
+available, or uses 4000 otherwise when running locally on our machines.
+*/
+app.listen(process.env.PORT || 4001);
