e9f5430044037a736cf7567314a6268dbd061cf1
sarzig <63985693+sarzig@users.noreply.github.com>
2024-04-16 21:25:13 -0400

    finished 3 but is real bad

--- Users/dao.js
+++ Users/dao.js
@@ -1,8 +1,14 @@
 import model from "./model.js";
-export const createUser = (user) => model.create(user);
+
+export const createUser = (user) => {
+    delete user._id // remove _id field just in case client sends it. Database will create _id for us instead
+
+    return model.create(user);
+};
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
-export const findUserByUsername = (username) =>  model.findOne({ username: username });
-export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
-export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ _id: userId }); 
+export const findUserByUsername = (username) => model.findOne({ username: username });
+export const findUserByCredentials = (username, password) => model.findOne({ username, password });
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByRole = (role) => model.find({ role: role });
