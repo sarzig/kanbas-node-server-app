219758ab6b10c8f2e8667d90da72fe45ce0886ca
sarzig <63985693+sarzig@users.noreply.github.com>
2024-04-16 20:14:43 -0400

    init mongoose stuff

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
 import cors from "cors";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import UserRoutes from "./Users/routes.js";
+
+// connect to the kanbas database
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 // Set up Express application
 const app = express();
@@ -14,11 +19,13 @@ app.use(express.json());
 app.use(cors());
 
 // Register routes
-CourseRoutes(app);
+Lab5(app);              // lab
+Hello(app);
+CourseRoutes(app);      // Kanbas
 ModuleRoutes(app);
 AssignmentRoutes(app);
-Lab5(app);
-Hello(app);
+UserRoutes(app);        // Users
+
 
 // Server listening
 app.listen(process.env.PORT || 4001);
